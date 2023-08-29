f0818dbc48305b0a2e9f3ef330b37388390476ff
nomnivore <6979410+nomnivore@users.noreply.github.com>
2023-08-28 20:03:18 -0400

    FEN parsing

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,12 @@
-function example() {
-  return 1;
-}
+import { Engine } from "../src/index.js";
 
-describe(example, () => {
-  it("should return 1", () => {
-    expect(example()).toBe(1);
+describe("Engine", () => {
+  it("should parse and serialize a FEN", () => {
+    const fen =
+      "rnbqk2r/pp2ppbp/2p2np1/3p4/3P4/2N2N2/PPP1PPPP/R1BQKB1R w KQkq - 0 7";
+
+    const engine = new Engine(fen);
+
+    expect(engine.toFen()).toBe(fen);
   });
 });
